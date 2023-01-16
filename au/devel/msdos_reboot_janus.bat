794280d4351e4806e686303a210799e296078342
Duncan Howe <djh35@cam.ac.uk>
2023-01-16 13:35:31 +0000

    msdos scripts to handle janus restart

--- au/devel/msdos_reboot_janus.bat
+++ au/devel/msdos_reboot_janus.bat
@@ -3,5 +3,5 @@ SET file="z:\chemist\rebooting.txt"
 SET spect="arran"
 IF EXIST %file% (
   ECHO %file% is existing
-  shutdown /r /t 120 /c %spect%_automation_starting /d p:0:0
+  shutdown /r /t 5 /c %spect%_automation_starting /d p:0:0
 ) 
---
+++ au/devel/msdos_start_janus_submuission_pc.bat
@@ -0,0 +1,6 @@
+C:\nmrkiosk\nmrkiosk.exe
+REM now start infinite loop
+:l
+TIMEOUT /T 60
+c:\msdos_scripts\msdos_reboot_janus.bat
+goto l

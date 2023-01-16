a220c540a46278478ac6fd5bcad0fe779d69d4a0
Duncan Howe <djh35@cam.ac.uk>
2023-01-16 15:20:42 +0000

    msdos scripts to handle janus restart

--- au/devel/msdos_reboot_janus.bat
+++ au/devel/msdos_reboot_janus.bat
@@ -1,6 +1,6 @@
-SET file="y:\chemist\rebooting.txt"
+SET file="y:\rebooting.txt"
 SET spect="arran"
 IF EXIST %file% (
   ECHO %file% is existing
-  shutdown /r /t 5 /c %spect%_automation_starting /d p:0:0
+  shutdown /r /t 120 /c %spect%_automation_starting /d p:0:0
 ) 
--- au/devel/msdos_start_janus_submission_pc.bat
+++ au/devel/msdos_start_janus_submission_pc.bat
@@ -1,4 +1,12 @@
 SET file="y:\rebooting.txt"
+SET spect=arran
+IF EXIST %file% (
+  ECHO %file% is existing
+  DEL %file%
+) 
+REM above so we don't reboot loop.
+REM If the janus pc loses connection, we can trigger msdos_reboot on arran
+
 cd c:\nmrkiosk\
 start /b nmrkiosk.exe
 REM now start infinite loop
@@ -8,7 +16,7 @@ cd c:\msdos_scripts\
 REM c:\msdos_scripts\msdos_reboot_janus.bat
 IF EXIST %file% (
   ECHO %file% is existing
-  shutdown /r /t 5 /c %spect%_automation_starting /d p:0:0
+  shutdown /r /t 180 /c %spect%_automation_starting /d p:0:0
 ) 
 goto l
 

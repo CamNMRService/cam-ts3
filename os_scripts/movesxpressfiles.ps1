1bc832f2fb88e154e886cbb85566c1354e8accf4
PeteGierth <ptg20@cam.ac.uk>
2024-04-05 11:06:24 +0100

    Powershell script for sorting sxpress picture files

---
+++ os_scripts/movesxpressfiles.ps1
@@ -0,0 +1,25 @@
+# set this as appropriate
+$FolderPath = "C:\Users\chemist\SXpresslog"
+
+# Get all files in the specified folder
+$FilesToMove = Get-ChildItem -Path $FolderPath | Where-Object { $_.Name -like "*@ewpres*" }
+
+# Create subfolders based on modification date
+foreach ($file in $FilesToMove) {
+    $date = Get-Date ($file.LastWriteTime)
+    $year = $date.Year
+    $month = $date.Month
+    $day = $date.Day
+    $MonthPath = "$FolderPath\$year\$month\$day"
+
+    if (!(Test-Path -Path $MonthPath)) {
+        New-Item -ItemType directory -Path $MonthPath | Out-Null
+    }
+
+    # Rename the file with modification date and time (including seconds) and .png extension
+    $newFileName = "{0:yyyyMMdd-HHmmss}.png" -f $date
+    $newFilePath = Join-Path -Path $MonthPath -ChildPath $newFileName
+    Move-Item -Path $file.FullName -Destination $newFilePath -Force
+}
+
+Write-Host "All files have been sorted into subfolders based on modification date and renamed."

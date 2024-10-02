5047a055ef269df1a85023aec452ec9ed4adb312
CamNMRService <nmr@ch.cam.ac.uk>
2024-10-02 08:18:00 +0100

    janus_check.bat

--- os_scripts/janus_check.bat
+++ os_scripts/janus_check.bat
@@ -1,9 +1,12 @@
+
+echo off
+cls
 set file="e:\chemist\janus_running.txt"
 set txt="Janus is running."
-wmic process get Description | findstr /I "Janus"
+wmic process get Description | findstr /I "Janus_sx"
 if %errorlevel% == 0 (
     echo %txt% > %file%
-    wmic process get ProcessId,Description,ParentProcessId,ReadOperationCount,WriteOperationCount | findstr /I "Janus" > %file%
+    wmic process get ProcessId,Description,ParentProcessId,ReadOperationCount,WriteOperationCount | findstr /I "Janus_sx" > %file%
 ) else (
     echo Not running Janus
 )
